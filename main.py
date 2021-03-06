93cc2740549a190fef47921886a371b02006faaa
Paguiar735 <pvads.eng19@uea.edu.br>
2021-03-06 00:23:56 +0000

    Semana 2 Concluída

--- main.py
+++ main.py
@@ -44,16 +44,27 @@ def make_dir(a):
   if not os.path.exists(dir):
       os.mkdir(dir)
 
+# Encontrar último Índice
+def get_last_index():
+  dir = os.path.join(os.getcwd(), "Planilhas")
+  a = 1
+  while os.path.isfile(os.path.join(dir, str(a) + "-FILE" + ".csv")):
+    a = int(a) + 1
+  return a
+
 # Salvar CSV
 def save_CSV(df):
   make_dir("Planilhas")
+  a = get_last_index()
   save_path = os.path.join(os.getcwd(), "Planilhas")
-  completeName = os.path.join(save_path, "FILE" + ".csv")
+  completeName = os.path.join(save_path, str(a) + "-FILE" + ".csv")
   df.to_csv (completeName, index = False, header=True)
+
 
 # Iniciar Programa
 def main():
   save_CSV(read_spreadsheet(extract_ID()))
+
 
 #
 ##
@@ -67,48 +78,6 @@ def main():
 
 main()
 
-'''
-# operador para contar o sufixo do arquivo
-c = itertools.count()
-
-# ------------ DEMONSTRATIVO ------------ (APAGAR)#
-                                                  #
-if os.path.exists(completename):                  #
-    print("o arquivo ja existe no diretorio:")    #
-    print(completename)                           #
-else:                                             #
-    print("o arquivo é novo nesse diretorio")     #
-                                                  #
-# ------------------------------------------------#
-
-
-# sondar o diretorio em busca do nome do arquivo
-# enquanto o nome do arquivo existir, c = c + 1
-while os.path.exists(completename):
-    actualname = "%s (%d).%s" % (filename,next(c),ext)
-    completename = os.path.join(save_path, actualname)
-
-# quando o nome for novo no diretorio, salvar arquivo
-
-file1 = open(completename,"w",newline="")
-file1.write(df.to_csv(index=False))           # remove o prefixo (0, 1, 2...) da planilha
-file1.close()                                 # encerra a edição da planilh
-
-
-# ------------ DEMONSTRATIVO ------------ (APAGAR)#
-                                                  #
-print("")                                         #
-print("salvando como:")                           #
-print(completename)                               #
-                                                  #
-print("")                                         #
-print("arquivo csv salvo:")                       #
-print(df.to_string(index=False))                  #
-                                                  #
-# ------------------------------------------------#
-
-
-# (x) SALVAR OS ARQUIVOS CSV EM UMA PASTA, NÃO NO MESMO DIRETÓRIO DO ARQUIVO PYTHON
-# (x) SE ESTIVER COM O MESMO NOME, NÃO SOBRESCREVER, E SIM SALVAR COM OUTRO NOME (PLANILHA_1 -- > PLANILHA_2)
-# ( ) SE HOUVER MODIFICAÇÕES, SALVAR AS MODIFICAÇÕES EM UM ARQUIVO DE TEXTO
-'''
+# 1 - (x) SALVAR OS ARQUIVOS CSV EM UMA PASTA, NÃO NO MESMO DIRETÓRIO DO ARQUIVO PYTHON
+# 2 - (x) SE ESTIVER COM O MESMO NOME, NÃO SOBRESCREVER, E SIM SALVAR COM OUTRO NOME (PLANILHA_1 -- > PLANILHA_2)
+# 3 - ( ) SE HOUVER MODIFICAÇÕES, SALVAR AS MODIFICAÇÕES EM UM ARQUIVO DE TEXTO
